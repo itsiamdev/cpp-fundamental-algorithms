15e5bbc0ffb09d13f259eafcfbd5424e2ba318b7
Aurel Ionita <ionitaaurel32@gmail.com>
2026-07-01 19:16:18 +0300

    Add basic arithmetic algorithms for addition, subtraction, multiplication, and division

---
+++ basic-arithmetic/addition.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*
+    PROBLEM STATEMENT:
+
+    Given two integers a and b, compute their sum.
+*/
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << a + b << endl;
+
+    return 0;
+}
---
+++ basic-arithmetic/multiplication.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*
+    PROBLEM STATEMENT:
+
+    Given two integers a and b, compute their product.
+*/
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << a * b << endl;
+
+    return 0;
+}
